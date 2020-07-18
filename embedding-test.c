a5beb58451b7cd636d55ff6a30e769a3b2f702dc
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-18 14:34:41 +0200

    Used Scheme as an evaluation engine

--- embedding-test.c
+++ embedding-test.c
@@ -8,10 +8,13 @@ int main(int argc, char *argv[]) {
 
   scm_init_guile();
   scm_c_eval_string("(define result \"Hello, from Scheme!\n\")");
-  
+    
+  SCM variable = scm_c_lookup("result");
+  SCM referenced = scm_variable_ref(variable);
+  char *cstring = scm_to_utf8_stringn(referenced, NULL);
 
-  SCM symbol_name = scm_from_utf8_symbol("result");
-  SCM result = scm_variable_ref(symbol_name);
+  fprintf(stderr, "Guile contains: '%s'", cstring);
+
 
   return 0;
 }
