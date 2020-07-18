d512eadf9aa313eb4be031ba559bba6498967a88
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-18 13:19:48 +0200

    Trying (unsuccessfully) to read a define from the interpreter

--- embedding-test.c
+++ embedding-test.c
@@ -7,7 +7,11 @@
 int main(int argc, char *argv[]) {
 
   scm_init_guile();
-  scm_c_eval_string("(display \"Hello, from Scheme!\n\")");
+  scm_c_eval_string("(define result \"Hello, from Scheme!\n\")");
+  
+
+  SCM symbol_name = scm_from_utf8_symbol("result");
+  SCM result = scm_variable_ref(symbol_name);
 
   return 0;
 }
